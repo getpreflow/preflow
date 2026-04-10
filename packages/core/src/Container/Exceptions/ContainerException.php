95af8265f590e31ee1034eddf39adeeb836cd89a
Steffen Meyer <myr.steffen@gmail.com>
2026-04-10 23:12:48 +0200

    feat(core): add DI container with PSR-11 compliance and autowiring

---
+++ packages/core/src/Container/Exceptions/ContainerException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Preflow\Core\Container\Exceptions;
+
+use Psr\Container\ContainerExceptionInterface;
+
+final class ContainerException extends \RuntimeException implements ContainerExceptionInterface
+{
+}
